5ef669a0e4013b79481b1175f4a6d7211ca500d1
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-01 00:42:26 +0100

    [taalapi] Set default DRF config

--- taalapi/taalapi/settings.py
+++ taalapi/taalapi/settings.py
@@ -97,3 +97,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST framework configuration
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
