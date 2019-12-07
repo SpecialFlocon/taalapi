1295bba27033f6fa30b80b1656fc49573fe40bd1
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-07 02:59:21 +0100

    [taalapi] Fix an oversight in database settings

--- taalapi/taalapi/settings.py
+++ taalapi/taalapi/settings.py
@@ -79,10 +79,11 @@ WSGI_APPLICATION = 'taalapi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.{}'.format(os.environ.get('DATABASE_ENGINE')),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
         'NAME': os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
