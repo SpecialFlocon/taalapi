d41e736cb2e0753fad2914edf34176b86858fea9
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-12-10 00:57:36 +0100

    Turn search view into a purely learning view
    It no longer returns the top five results, only the word if there is an
    exact match.

--- src/knowledgebase/views.py
+++ src/knowledgebase/views.py
@@ -23,34 +23,38 @@ class WoordenViewSet(viewsets.ModelViewSet):
     queryset = Woord.objects.all()
     serializer_class = WoordSerializer
 
-    @action(detail=False, methods=['get'], url_path='search/(?P<query>[a-zA-Z]+)')
-    def search(self, request, query):
+    @action(detail=False, methods=['get'], url_path='learn/(?P<query>[a-zA-Z]+)')
+    def learn(self, request, query):
+        # First, query database to see if the word is already there, and return it if it does.
+        w = Woord.objects.filter(woord=query)
+        if w.exists():
+            serializer = self.get_serializer(w.first())
+            return Response(serializer.data)
+
+        # Otherwise, attempt to learn word
         h = WoordenlijstHelper()
-        matching_words = Woord.objects.filter(woord__contains=query)[:5]
+        helper_article_list = []
+        helper_response = h.get(query)
 
-        # If no matches were returned, attempt to learn word
-        if len(matching_words) == 0:
-            helper_article_list = []
-            helper_response = h.get(query)
+        # If first value of the tuple is 0 (no error), extract article(s)
+        # TODO(thepib): do something useful with failure values
+        if helper_response[0] == 0:
+            helper_article_list = helper_response[1]
+            article_list = [Lidwoord.objects.get(lidwoord=a).id for a in helper_article_list]
 
-            # If first value of the tuple is 0 (no error), extract article(s)
-            # TODO(thepib): do something useful with failure values
-            if helper_response[0] == 0:
-                helper_article_list = helper_response[1]
-                article_list = [Lidwoord.objects.get(lidwoord=a).id for a in helper_article_list]
+            word = Woord.objects.create(woord=query, accurate=helper_response[2])
+            word.lidwoord.add(*article_list)
 
-                word = Woord.objects.create(woord=query, accurate=helper_response[2])
-                word.lidwoord.add(*article_list)
-
-                # Somewhat dirty trick to pick up the newly added word
-                matching_words = Woord.objects.filter(id=word.id)
-            # If first value of the tuple is -1 (fetch error), HTML parsing code be faililng, yield server error
-            elif helper_response[0] == -1:
-                data = {'error': helper_response[1]}
-                return Response(data=data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            # If first value of the tuple is 1 (word not found), bail out with a 404
-            elif helper_response[0] == 1:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = self.get_serializer(matching_words, many=True)
-        return Response(serializer.data)
+            serializer = self.get_serializer(word)
+            return Response(serializer.data)
+        # If first value of the tuple is -1 (fetch error), HTML parsing code be faililng, yield server error
+        elif helper_response[0] == -1:
+            data = {'error': helper_response[1]}
+            return Response(data=data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # If first value of the tuple is 1 (word not found), bail out with a 404
+        elif helper_response[0] == 1:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        # Unreachable code
+        else:
+            data = {'error': 'Got unexpected return value from helper: {}'.format(helper_response[0])}
+            return Response(data=data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
